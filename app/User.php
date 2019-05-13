fc52c24f317d9ec45bfdbb11dd50eaf5767bff9b
harukaze.yudachi <kirameki0hanako@gmail.com>
2019-05-13 08:11:35 +0000

    timeline2

--- app/User.php
+++ app/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     
+    
         public function microposts()
     {
         return $this->hasMany(Micropost::class);
@@ -38,6 +39,11 @@ class User extends Authenticatable
     }
 
     public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+        public function favorites()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
     }
@@ -76,15 +82,47 @@ class User extends Authenticatable
         }
     }
     
-    public function is_following($userId)
+        public function is_following($userId)
     {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
-
-    public function feed_microposts()
+    
+        public function feed_microposts()
     {
         $follow_user_ids = $this->followings()->pluck('users.id')->toArray();
         $follow_user_ids[] = $this->id;
         return Micropost::whereIn('user_id', $follow_user_ids);
     }
+    
+        public function favorite($userId)
+    {
+        // 既にお気に入りにしているかどうかの確認
+        $exist = $this->is_favorite($userId);
+        // 相手が自分自身ではないかの確認
+        $its_me = $this->id == $userId;
+    
+        if ($exist || $its_me) {
+            return false;
+        } else {
+            $this->favorites()->attach($userId);
+            return true;
+        }
+        
+    }
+    
+        public function unfavorite($userId)
+    {
+        // 既にフォローしているかの確認
+        $exist = $this->is_favorite($userId);
+        // 相手が自分自身ではないかの確認
+        $its_me = $this->id == $userId;
+    
+        if ($exist && !$its_me) {
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }
